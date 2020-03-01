dba80c258766f8ae6362308ab96cf56f48930d6d
Dom Grisafe <dgrisafe@users.noreply.github.com>
2020-03-01 15:47:55 -0800

    Documentation for formatting and table functions

--- R/formats.R
+++ R/formats.R
@@ -1,20 +1,54 @@
-# function for formatting numbers
-trimn <- function(x, n = 1){sprintf(paste0("%.", n, "f"), round(as.double(x), n))}
+#' Trim Number
+#'
+#' Format number to n decimal places
+#'
+#' @param x Numeric to be formatted.
+#' @param n_dec Numeric for number of digits to format after the decimal.
+#' @return The formatted \code{x}.
+#' @examples
+#' trim_n(pi, 3)
+#' trim_n(1.2345, 1)
+trim_n <- function(x, n_dec = 1){sprintf(paste0("%.", n_dec, "f"), round(as.double(x), n_dec))}
 
 
-# function to standardize variables
-std_var <- function(x){(x - mean(x, na.rm = T)) / sd(x, na.rm = T)}
-
-
-# function that creates pretty estimates with lower and upper confidence limits
-pretty_estimate <- function(estimate, limit_low, limit_high, n_round = 3){
+#' Pretty Estimate
+#'
+#' Create character string of an estimate with its confidence limits.
+#'
+#' @param estimate Numeric estimate.
+#' @param limit_low Numeric estimate lower confidence interval.
+#' @param limit_high Numeric estimate higher confidence interval.
+#' @param n_dec Numeric indicating number of digits to round after decimal.
+#' @return Character string of formatted estimate.
+#' @examples
+#' iris_estimate   <- mean(iris$Petal.Length)
+#'  iris_std_err   <- sd(iris$Petal.Length) / sqrt(length(iris$Petal.Length))
+#' iris_limit_low  <- iris_estimate - 1.96 * iris_std_err
+#' iris_limit_high <- iris_estimate + 1.96 * iris_std_err
+#'
+#' pretty_estimate(iris_estimate, iris_limit_low, iris_limit_high, 2)
+pretty_estimate <- function(estimate, limit_low, limit_high, n_dec = 3){
 
   df_estimate <- c(estimate, limit_low, limit_high)
 
   # trim variables to n decimals
-  pretty_estimate <- map_chr(.x = df_estimate, .f = trimn, n_round)
+  pretty_estimate <- map_chr(.x = df_estimate, .f = trim_n, n_dec)
 
   # return pretty estimate
   paste0(pretty_estimate[[1]], " (", pretty_estimate[[2]], ", ", pretty_estimate[[3]], ")")
 
 }
+
+
+#' Standardize Variable
+#'
+#' Standardize variable by centering on its mean and dividing by its standard deviation.
+#' The new variable will now have a mean of 0 and a standard deviation of 1.
+#' All missing data points are excluded from the mean and deviation calculations.
+#'
+#' @param x Numeric variable to be formatted.
+#' @return The standardized \code{x}.
+#' @examples
+#' std_var(iris$Petal.Length)
+#' std_var(cars$speed)
+std_var <- function(x){(x - mean(x, na.rm = T)) / sd(x, na.rm = T)}
--- R/tables.R
+++ R/tables.R
@@ -1,4 +1,17 @@
-# function for a quick and dirty html table (kable())
+#' Quick Kable
+#'
+#' Create table with default settings for kableExtra::kable() function.
+#' Produces table in R Markdown.
+#'
+#' @param df Dataframe to be converted to table.
+#' @param p_digits Numeric for maximum number of digits to round numeric values.
+#' @param p_format Character string. Possible values are latex, html, markdown, pandoc, and rst; this will be automatically determined if the function is called within knitr; it can also be set in the global option knitr.table.format. If format is a function, it must return a character string.
+#' @param lab_title Character for table title.
+#' @param col_names Character for column names. Must have the same length as the number of columns in the dataframe.
+#' @return The table ready for output in R Markdown.
+#' @examples
+#' quick_kable(iris)
+#' quick_kable(iris, lab_title = "Cool HTML Table of Iris Data", col_names = c("S Length", "S Width", "P Length", "P Width", "Species"))
 quick_kable <- function(df, p_digits = 3, p_format = "html", lab_title = "", col_names = NULL){
 
   # if no column names are specified, give original column names
@@ -7,10 +20,32 @@ quick_kable <- function(df, p_digits = 3, p_format = "html", lab_title = "", col
   else{save_col_names <- col_names}
 
   df %>%
-    kable(digits = p_digits, format = p_format, caption = lab_title, col.names = save_col_names, escape = F) %>%
+    kable(digits = p_digits,
+          format = p_format,
+          caption = lab_title,
+          col.names = save_col_names,
+          escape = F
+          ) %>%
     kable_styling(bootstrap_options = c("striped", "hover"))
 
 }
 
-# function for formatting p-values in kable
-form_kable_p_value <- function(var_p_value){cell_spec(sprintf("%.3f", round(var_p_value, 3)), "html", color = ifelse(var_p_value < 0.05, "red", ifelse(var_p_value < 0.15, "orange", "black")))}
+#' Colorful P-Values
+#'
+#' Color the p-values in html so they change colors conditional on values.
+#'    Red indicates p-values less than 0.05.
+#' Orange indicates p-values less than 0.15.
+#' @param var_p_value Numeric variable of p-values to be colored conditionally.
+#' @return New character string variable with html code for color of p-value.
+#' @examples
+#' set.seed(2020)
+#' data <- data_frame(p_val = sort(sample(seq(0, 0.22, 0.01), replace = T, size = 10)))
+#' data$p_val_html <- color_p_value(data$p_val)
+#' quick_kable(data)
+color_p_value <- function(var_p_value){
+  cell_spec(
+    sprintf("%.3f", round(var_p_value, 3)),
+    "html",
+    color = ifelse(var_p_value < 0.05, "red", ifelse(var_p_value < 0.15, "orange", "black"))
+    )
+}
